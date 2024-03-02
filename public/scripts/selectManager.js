b981d173b5f7567620f0482cd40b50fa8790d87a
SmartWavesCafe <smartwavescafe@gmail.com>
2024-03-02 15:39:10 +0100

    a

--- public/scripts/selectManager.js
+++
@@ -1,7 +0,0 @@
-const dropdown = document.getElementById('festes');
-const input = document.getElementById('datum');
-
-dropdown.addEventListener('change', function() {
-    const selectedValue = dropdown.value;
-    input.value = selectedValue
-})
